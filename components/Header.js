0ee7ba80376136a4a6d33a78b6fe2343822c1025
Diemas Michiels <michielsdiemas@gmail.com>
2018-11-28 22:04:25 +0100

    Module 6 shopping cart

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import Router from 'next/router'
 import NProgess from 'nprogress'
 import styled from 'styled-components'
 import Nav from './Nav'
+import Cart from './Cart'
 
 Router.onRouteChangeStart = () => {
   NProgess.start()
@@ -65,9 +66,7 @@ const Header = () => (
     <div className='sub-bar'>
       <p>Search</p>
     </div>
-    <div>
-      Cart
-    </div>
+    <Cart />
   </StyledHeader>
 )
 
