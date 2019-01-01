84b006bb14c97a6d874081006af0ed60777738d2
Diemas Michiels <michielsdiemas@gmail.com>
2019-01-01 16:09:24 +0100

    Module 8: Credit Card Checkout

--- components/Cart.js
+++ components/Cart.js
@@ -10,6 +10,7 @@ import CartStyles from './styles/CartStyles'
 import Supreme from './styles/Supreme'
 import CloseButton from './styles/CloseButton'
 import SickButton from './styles/SickButton'
+import TakeMyMoney from './TakeMyMoney'
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -46,7 +47,11 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            {
+              me.cart.length && (<TakeMyMoney>
+                <SickButton>Checkout</SickButton>
+              </TakeMyMoney>)
+            }
           </footer>
         </CartStyles>
       )
